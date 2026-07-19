62650f98c5c1663bc6c32033add21e4748510f80
Bayasgalan Erdenebat <bbaysgalan911@gmail.com>
2026-07-19 17:41:04 +0900

    feat: initial spring boot setup with postgres jpa and thymeleaf

---
+++ src/test/kotlin/com/example/msavoting/MsaVotingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.msavoting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsaVotingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
